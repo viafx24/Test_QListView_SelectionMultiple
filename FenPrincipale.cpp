5ed6529dcc536b30b341ba2dae3308e383e1db1a
Guillaume Baptist <guillaume.baptist@free.fr>
2020-11-01 18:02:01 +0100

    ajout de commentaire

--- FenPrincipale.cpp
+++ FenPrincipale.cpp
@@ -23,8 +23,14 @@ FenPrincipale::FenPrincipale()
 
 void FenPrincipale::clicSelection()
 {
+
+    // C'est ces quatres lignes qui sont  un peu lourdes
+    // on récupère la selection
     QItemSelectionModel *selection = vue->selectionModel();
+    // on récupère l'index (numero de l'element selection)
     QModelIndex indexElementSelectionne = selection->currentIndex();
+    // on utilise data pour retrouver le texte à partir de l'index
     QVariant elementSelectionne = modele->data(indexElementSelectionne, Qt::DisplayRole);
+    // on affiche la boite de dialogue avec le string récupéré
     QMessageBox::information(this, "Elément sélectionné", elementSelectionne.toString());
 }
